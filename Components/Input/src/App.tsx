4fc91baabdc3b8ebb0681e061ec5cee0f759960e
grvbrk <gouravbarikofficial@gmail.com>
2024-01-21 00:03:17 +0530

    Major functionality added 90& done

--- Components/Input/src/App.tsx
+++ Components/Input/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Option from "./components/Option";
 import Chip from "./components/Chip";
 import avatarImage from "/avatar.png";
 import { v4 as uuidv4 } from "uuid";
 
-const options = [
+const OPTIONS = [
   {
     name: "First",
     age: 21,
@@ -36,40 +36,82 @@ const options = [
 ];
 
 function App() {
-  const [option, setOption] = useState(options);
-  const [showOption, setShowOptions] = useState(false);
+  const [option, setOption] = useState(OPTIONS);
+  const [showOptions, setShowOptions] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const [optionCount, setOptionCount] = useState(OPTIONS.length);
+
+  useEffect(() => {
+    const options = option.filter((item) => item.isSelected === false);
+    setOptionCount(options.length);
+  }, [option]);
 
   function optionSelected(id: string) {
-    const newOption = [...option].map((item)=>(
-      item.id === id ? {...item, isSelected : !item.isSelected} : item
-    ))
-    setOption(newOption)
+    const newOption = [...option].map((item) =>
+      item.id === id ? { ...item, isSelected: !item.isSelected } : item
+    );
+    setOption(newOption);
   }
 
-  function handleClick() {
-    setShowOptions(!showOption);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setShowOptions(true);
+    const newOption = [...OPTIONS].filter((item) =>
+      item.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setOption(newOption);
   }
 
-  // function handleBlur() {
-  //   setShowOptions(false);
-  // }
+  const handleKeyPress = useCallback(
+    function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+      if (e.code === "Enter") {
+        if (!showOptions) setShowOptions(true);
+        if (showOptions) {
+          const options = option.filter((item) => item.isSelected === false);
+          const selectedOptionId = options[highlightedIndex].id;
+          optionSelected(selectedOptionId);
+        }
+      }
+      if (e.code === "ArrowDown") {
+        const newIndex = highlightedIndex + 1;
+        if (newIndex >= 0 && newIndex < optionCount) {
+          setHighlightedIndex(newIndex);
+        }
+      }
+      if (e.code === "ArrowUp") {
+        const newIndex = highlightedIndex - 1;
+        if (newIndex >= 0 && newIndex < optionCount) {
+          setHighlightedIndex(newIndex);
+        }
+      }
+    },
+    [optionCount, highlightedIndex]
+  );
 
   return (
     <>
       <div className="container">
         <div className="input-container">
-          <Chip optionSelected={optionSelected} option={option}/>
+          <Chip optionSelected={optionSelected} option={option} />
           <input
-            onClick={handleClick}
-            // onBlur={handleBlur}
+            onClick={() => {
+              setHighlightedIndex(0);
+              setShowOptions(!showOptions);
+            }}
+            onBlur={() => setShowOptions(false)}
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
             className="input"
             type="text"
             placeholder="Choose"
           />
         </div>
         <div className="render-options">
-          {showOption && (
-            <Option optionSelected={optionSelected} option={option} />
+          {showOptions && (
+            <Option
+              optionSelected={optionSelected}
+              option={option}
+              highlight={highlightedIndex}
+            />
           )}
         </div>
       </div>
--- Components/Input/src/components/Option.tsx
+++ Components/Input/src/components/Option.tsx
@@ -11,19 +11,22 @@ export type optionType = {
 type propsType = {
   option: optionType[];
   optionSelected: (value: string) => void;
+  highlight : number
 };
 
-function Option({ option, optionSelected }: propsType) {
-
-  const notSelectedOptions = useMemo(()=>{
-    return option.filter((item)=>(item.isSelected === false))
-  }, [option])
-
+function Option({ option, optionSelected, highlight }: propsType) {
+  const notSelectedOptions = useMemo(() => {
+    return option.filter((item) => item.isSelected === false);
+  }, [option]);
 
   return (
     <div className="options-container">
-      {notSelectedOptions.map(({ id, name, age, image }) => (
-        <button className="options" key={id} onClick={() => optionSelected(id)}>
+      {notSelectedOptions.map(({ id, name, age, image }, index) => (
+        <button
+          className={`options ${highlight === index ? "highlight" : null}`}
+          key={id}
+          onMouseDown={() => optionSelected(id)}
+        >
           <img src={image} alt="avatar" />
           <p>{name}</p>
           <p>{age}</p>
