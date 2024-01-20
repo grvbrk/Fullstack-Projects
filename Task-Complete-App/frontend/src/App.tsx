ab84e649c50dfcca84c331f1238d6f41faf44375
grvbrk <gouravbarikofficial@gmail.com>
2024-01-20 19:24:43 +0530

    frontend

--- Task-Complete-App/frontend/src/App.tsx
+++ Task-Complete-App/frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes} from "react-router-dom"
+import { Route, Routes, Navigate} from "react-router-dom"
+import {Input} from "./components/Input.tsx"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="/" element={<Input />}/>
+        <Route path="*" element={<Navigate to={"/"}/>}/>
       </Routes>
     </>
   );
---
+++ Task-Complete-App/frontend/src/components/Input.tsx
@@ -0,0 +1,12 @@
+
+
+function Input() {
+  return(
+  <div>
+    <h1 className="text-center">Hello</h1>
+    <input className="bg-blue-800 border-blue-400" type="text" />
+  </div>
+  )
+}
+
+export { Input };
