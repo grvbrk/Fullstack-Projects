7b58ca4ad1cd239057bfece161a77ab57a68baa1
grvbrk <gouravbarikofficial@gmail.com>
2024-02-02 22:53:22 +0530

    be

--- Hotel-Booking/backend/index.ts
+++ Hotel-Booking/backend/index.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./db/connect";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.get("/", (req, res)=>{
-  return res.json({msg: "Success"})
-})
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  return res.json({ msg: "Success" });
+});
 
-app.listen(PORT, () => console.log("Server is running on PORT 3001"));
+async function start() {
+  try {
+    connectDB(process.env.MONGO_KEY as string);
+    app.listen(PORT, () => console.log("Server is running on PORT 3001"));
+  } catch (error) {
+    console.log("Error connecting to mongoDB");
+  }
+}
+
+start()
